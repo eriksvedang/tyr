80b6b52340f42c336a2ad48e47420b81a13bf7cd
Erik Svedäng <erik.svedang@gmail.com>
2024-11-13 22:52:41 +0100

    One test.

--- src/ty.rs
+++ src/ty.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Ty {
     Var(String),
     Func(Vec<Ty>, Box<Ty>), // TODO: support type variable constraints
@@ -28,5 +28,5 @@ pub enum Kind {
 /// The consumer of the library is free to map their concrete types
 /// (e.g. 'f32', or 'String') to any Id they see fit - or they
 /// can use the tyr::TypeMapper for convenience.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct TypeId(pub usize);
--- src/unifier.rs
+++ src/unifier.rs
@@ -1,15 +1,52 @@
 #![allow(dead_code)]
 
+use std::collections::HashMap;
+
 use crate::ty::Ty;
 
 #[derive(Debug, Clone)]
 pub struct Unifier {
     constraints: Vec<Constraint>,
+    solved_variables: HashMap<String, Ty>,
 }
 
 impl Unifier {
-    pub fn new(constraints: Vec<Constraint>) -> Self {
-        Self { constraints }
+    pub fn new() -> Self {
+        Self {
+            constraints: Vec::new(),
+            solved_variables: HashMap::new(),
+        }
+    }
+
+    pub fn add_constraint(&mut self, constraint: Constraint) {
+        self.constraints.push(constraint);
+    }
+
+    pub fn solve(&mut self) -> Result<(), ()> {
+        // TODO: fix cloning
+        for constraint in self.constraints.clone() {
+            self.solve_one(&constraint)?;
+        }
+
+        Ok(())
+    }
+
+    fn solve_one(&mut self, constraint: &Constraint) -> Result<(), ()> {
+        match &constraint.a {
+            Ty::Var(name) => {
+                _ = self
+                    .solved_variables
+                    .insert(name.to_string(), constraint.b.clone())
+            }
+            Ty::Func(_, _) => (),
+            Ty::Data(_, _) => (),
+        }
+
+        Ok(())
+    }
+
+    pub fn get_solved(&self, variable_name: &str) -> Option<&Ty> {
+        self.solved_variables.get(variable_name)
     }
 }
 
@@ -22,5 +59,23 @@ pub struct Constraint {
 impl Constraint {
     pub fn new(a: Ty, b: Ty) -> Self {
         Self { a, b }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ty::TypeId;
+
+    use super::*;
+
+    #[test]
+    fn unify_var_with_concrete_type() {
+        let mut unifier = Unifier::new();
+        unifier.add_constraint(Constraint::new(
+            Ty::Var("a".to_string()),
+            Ty::Data(TypeId(0), vec![]),
+        ));
+        _ = unifier.solve();
+        assert_eq!(unifier.get_solved("a"), Some(&Ty::Data(TypeId(0), vec![])));
     }
 }
